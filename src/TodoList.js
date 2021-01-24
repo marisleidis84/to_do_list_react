0516bbadc88f674bee84fe0b927d46335c826cca
marisleidids84 <marisleidids84@gmail.com>
2021-01-24 17:25:26 -0300

    Terminado to-do-lest-react-fetch

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,27 +3,83 @@ import './App.css';
 
 const TodoList = () => {
     const [task, setTask] = useState([]);
-    const [inputValor, setinputValor] = useState('');
+    let [inputValor, setinputValor] = useState('');
 
+     
+    //Aqui se agrega un nevo suario
     const addTask = () => {
-        if (inputValor !== '') {
-            setTask(task.concat(inputValor));
-        }
-        document.getElementById("prueba").value = '';
+        let data = [{
+            "label": "Hacer mercado",
+            "done": false
+        },
+        {
+            "label": "Pasear al perro",
+            "done": false
+        },
+        {
+            "label": "Limpiar la cocina",
+            "done": false
+        },
+        {
+            "label": "Sacar la basura",
+            "done": false
+        }];
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${inputValor}`, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(resp => {
+                console.log(resp);
+                return resp.json();
+            })
+            .then(data => {
+                if (inputValor !== '') {
+                    setTask(task.concat(inputValor));
+                    data = task;
+                }
+                document.getElementById("prueba").value = '';
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
-
+   
     const trash = y => {
-        const newTasks = [...task];
+        // Aqui se elimina el suario
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${task[y]}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(resp => {
+            return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+        })
+        .then(data => {
+            //here is were your code should start after the fetch finishes
+            console.log(data); //this will print on the console the exact object received from the server
+            const newTasks = [...task];
         newTasks.splice(y, 1);
         setTask(newTasks);
+        })
+        .catch(error => {
+            //error handling
+            console.log(error);
+        });
     }
+
+
+
 
     return (
         <>
             <div className='container d-flex justify-content-center'>
                 <div className='row'>
                     <div className='col-md-12'>
-                        <input id='prueba' onChange={(e) => setinputValor(e.target.value)} />
+                        <input id='prueba' onChange={(e) => setinputValor(inputValor = e.target.value)} />
                         <button type='button' onClick={addTask} >Agregar </button>
                     </div>
                 </div>
